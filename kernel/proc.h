9d54435a2c03e14205b9b1e86504789ff84e2653
yucheng_xiang <846735552@qq.com>
2024-02-25 13:44:17 +0800

    pgtbl: Q1 speed up system call and Q2 print page table

--- kernel/proc.h
+++ kernel/proc.h
@@ -106,4 +106,5 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   int mask; // use for trace system call
+  struct usyscall *usyscall;
 };
